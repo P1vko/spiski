1cd24ca3b2ad54c09714985d909ebe507574f7a4
P1vko <60178690+P1vko@users.noreply.github.com>
2020-08-06 16:04:11 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let input = document.querySelector('#start');
+let processingButton = document.querySelector(".processing-button");
+let outputUl = document.querySelector('.output-ul');
+
+const exist = param => param ? param : '';
+const lineConstr = (line) => {
+    let desc = '';
+    line.replace('/', ' ').replace('|', '').trim().split(' ').forEach((item,i) => {
+        if (i) {
+            desc = `${desc} ${exist(item)}`;
+        }
+    })
+    return desc;
+}
+
+
+processingButton.addEventListener('click', () => {
+    let data = input.value;
+    outputUl.innerHTML = '';
+    let arr = data.split('<');
+    arr.forEach((item, i) => {
+        if (i) {
+            const line = item.split(`": `)[1].replace(/\s+/g,' ').replace('/', ' ').replace('|', ' ').replace('\\', ' ');
+            const fullLine = `${line.split(' ')[0].replace('/', '').trim()} / ${lineConstr(line)}`.replace(/\s+/g,' ');
+            const code = `<li class="output-item">${fullLine}</li>`;
+            outputUl.insertAdjacentHTML('beforeEnd', code);
+        }
+    });
+});
